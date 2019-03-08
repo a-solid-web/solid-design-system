2874fb20473c90deca5a16ebdac56f9a527fdd55
Desislava Belokonska <desislava.belokonska@code.berlin>
2019-03-08 13:08:14 +0100

    Add eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb', 'prettier', 'react-app'],
+  plugins: ['prettier', 'react'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: false,
+    },
+  },
+};
