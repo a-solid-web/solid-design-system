35a6efbe55ad1f19bb0c7cc3d24722fe80bdde22
doom <denis.ismail@code.berlin>
2019-03-19 21:29:14 +0100

    Export all elements from index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Button from './elements/Button';
+import List from './elements/List';
+import ListItem from './elements/ListItem';
+import TextField from './elements/TextField';
+import Typography from './elements/Typography';
+
+export { Button, List, ListItem, TextField, Typography };
