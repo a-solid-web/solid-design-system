3a4f1449e6cf44aebf88e84ec0e37aee765cc461
Desislava Belokonska <desislava.belokonska@code.berlin>
2019-04-16 16:37:06 +0200

    Export missing Checkbox element

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Button from './elements/Button';
+import Checkbox from './elements/Checkbox';
 import List from './elements/List';
 import ListItem from './elements/ListItem';
 import TextField from './elements/TextField';
 import Toggle from './elements/Toggle';
 import Typography from './elements/Typography';
 
-export { Button, List, ListItem, TextField, Toggle, Typography };
+export { Button, Checkbox, List, ListItem, TextField, Toggle, Typography };
